04c7bea82ab0cfebb30c933f6e46edce09546dbe
ymk <2013627837@qq.com>
2019-11-14 17:36:49 +0800

    add 3

--- src/main/java/com/hyr/test/Ssm.java
+++ src/main/java/com/hyr/test/Ssm.java
@@ -5,7 +5,7 @@ public class Ssm {
 	public static void main(String[] args) {
 	System.out.println("1111");
 	System.out.println("2222");
-	System.out.println("3333");
+	System.out.println("33334");
 	}
 
 }
