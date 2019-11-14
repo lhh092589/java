4fdb693cf1cb566d2c142a40163fd950feb42048
ymk <2013627837@qq.com>
2019-11-14 17:34:46 +0800

    add 3333

--- src/main/java/com/hyr/test/Ssm.java
+++ src/main/java/com/hyr/test/Ssm.java
@@ -4,7 +4,8 @@ public class Ssm {
 
 	public static void main(String[] args) {
 	System.out.println("1111");
-	System.out.println("2222")
+	System.out.println("2222");
+	System.out.println("3333");
 	}
 
 }
